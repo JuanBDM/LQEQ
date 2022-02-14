b64a79cba118666afa48b1e08a2b5f7286e9b2d7
JuanBDM <jccarrizo.jcc@gmail.com>
2022-02-13 21:03:24 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,18 @@
+
+const express = require("express");
+const app = express();
+const path = require("path");
+const mainRouter = require("./routes/mainRouter");
+
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.listen(3000, () => {
+    console.log("El servidor de LQEQ esta corriendo en: http://localhost:3000/")
+});
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, "views"));
+
+app.use("/", mainRouter);
+
---
+++ public/js/nav.js
@@ -0,0 +1,13 @@
+/* Nav Bar */
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+    });
+   
+}
+
+navSlide();
+/* Nav Bar */
